f9e5ac20ec583d36701c9cbbe7cd5cfba6dcd1c0
Simon Nevzorov <sem.nevzorov@gmail.com>
2023-02-27 01:33:56 +0300

    Task4

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число :");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число :");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+Console.WriteLine("Максимальное число = " + max);
+
---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число :");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число :");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число :");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+// count =3
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(max);
+
