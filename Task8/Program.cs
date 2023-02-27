2bbd8b9234255399e32ead21593a2117e7d093d4
Simon Nevzorov <sem.nevzorov@gmail.com>
2023-02-27 08:49:59 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Console.WriteLine();
+Console.WriteLine("Четные числа до введенного числа: ");
+
+while (count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(" " + count);
+    }
+    count++;
+}
